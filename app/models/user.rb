dc88c253065e760caf3fd287c713786a1d9b4f25
Ryo Suzuki <ryooopan@googlemail.com>
2013-12-18 15:13:53 +0900

    Enable to save twitter user info in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,24 @@
 class User < ActiveRecord::Base
+
+  validates :uid, presence: true, uniqueness: true
+  validates :username, presence: true, uniqueness: true
+  validates :token, presence: true
+  validates :secret, presence: true
+
+  def self.oauth(auth)
+    uid = auth[:uid]
+    params = {
+      username: auth[:info][:nickname],
+      token: auth[:credentials][:token], 
+      secret: auth[:credentials][:secret] 
+    }
+    @user = User.find_by(uid: uid)
+    if @user.nil?
+      params = params.merge(uid: uid) 
+      @user = User.create(params)
+    else
+      @user.update(params)
+    end
+  end
+
 end
