9739b90af6fb526071e8ab3034b16361c26a8d20
Ryo Suzuki <ryooopan@googlemail.com>
2013-12-18 15:37:38 +0900

    Show tweet timeline in users#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(username: params[:id])
+    Twitter.configure do |config|
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+    end
+    @client = Twitter::Client.new(oauth_token: @user.token, oauth_token_secret: @user.secret)
+    @tweets = @client.user_timeline(@user.uid)
   end
 
   def destroy
